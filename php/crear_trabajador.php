7bc04ef3a5969205b2659e2941671efae1a5f89a
andres2000n <morenoandres761@gmail.com>
2021-03-23 11:39:13 -0500

    crear trabajadores con turnos

--- php/crear_trabajador.php
+++ php/crear_trabajador.php
@@ -10,12 +10,15 @@
         $tel = $_POST['telefono'];
         $maquina = $_POST['maquina_disponible'];
         $tipuser = 2;
+        // $est = "No trabajando";
+        // $turn = 1;
 
         if($tipo != "Nulo") {
             $sql = "INSERT INTO trabajadores (documento, nombre, apellido, telefono, id_tip_usu, id_tip_doc, id_maquina) VALUES ('$doc', '$nom', '$apellido', '$tel', '$tipuser', '$tipo', '$maquina')";
             $result = mysqli_query($conn, $sql);
+            
 
-            $sql2 = "INSERT INTO turnos (documento, id_maquina, hora_inicio, hora_fin, estado) VALUES ('$doc', '$maquina', NULL, NULL, 'No trabajando')";
+            $sql2 = "INSERT INTO `turnos` (`id_turno`, `documento`, `id_maquina`, `hora_inicio`, `hora_fin`, `estado`) VALUES (NULL, '$doc', '$maquina', NULL, NULL, NULL, 'No trabajando')";
             $result2 = mysqli_query($conn, $sql2);
             
             if($result) {
